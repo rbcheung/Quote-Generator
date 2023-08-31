3af23b091d4a55840cf3b92ca648392507d40ead
Ryan Cheung <ryan_cheung@hotmail.co.uk>
2023-08-31 23:26:35 +0100

    Added function so that the text content of dom elements change on page load.

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 // Get Random quotes from API
 
+// Selecting HTML elements using document.getElementById.
+
+const quoteText = document.getElementById("quote-text")
+console.log(quoteText)
+const quoteAuthor = document.getElementById("quote-author")
+console.log(quoteAuthor)
+const twitterBtn = document.getElementById("twitter-button")
+console.log(twitterBtn)
+const quoteBtn = document.getElementById("quote-button")
+console.log(quoteBtn)
+
 // Global variable created so that the response can be put into an empty string and used by the fetchQuotes function. I have used let because the data in the array will change.
 
 let ApiQuotes = []
 
-// new function added to randomise the quote.
-function randomiseQuote()  {
+// new function added to randomise the quote and populate page correctly.
+function newQuote()  {
     // New variable created to stored randomised quote. Math.random finds a random number. wrapped in Math.floor as this ensures theres no decimal it rounds down to the nearest whole number/integer. wrappred in square brackets as we are finding the index of the ApiQuotes array. multiply by length of apiQuotes array so that it will never be higher than the length of the array.
     const randomQuote = ApiQuotes[Math.floor(Math.random() * ApiQuotes.length)]
     console.log(randomQuote)
+
+    // Check to see if author in random quote is null. if null, change text content to read "unknown", if not proceed with whatever data the api data gives for the author of that random quote.
+    if (!randomQuote.author) {
+        quoteAuthor.textContent = "Unknown"
+    } else {
+        quoteAuthor.textContent = randomQuote.author
+    }
+
+    // check to ensure that text size gets smaller when the quote is too long.
+
+    if (randomQuote.text.length > 120)  {
+        quoteText.classList.add("long-quote")
+    } 
+
+    quoteText.textContent = randomQuote.text
+    quoteAuthor.textContent = randomQuote.author
+
+
 }
+
+
 
 async function fetchQuotes() {
     // API URL stored in variable.
@@ -20,7 +51,7 @@ async function fetchQuotes() {
         const response = await fetch(apiURL)
         // response turned into json so data can be read.
         ApiQuotes = await response.json()
-        randomiseQuote()
+        newQuote()
 
     } catch (error) {
 
